64588df2fcd54e284393d0f958db3ba0f2ffa7a8
Abhishek Kumar <abhi28535@gmail.com>
2024-06-09 15:58:23 +0530

    completed page

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -9,18 +9,18 @@ const Header = () => {
        <div className="flex justify-between items-center">
         <p className="text-lg lg:text-xl">TANN TRIM</p>
         <div className="flex gap-4"> 
-        <CiSearch size={24}/>
-        <BsPerson size={24}/>
-        <CiBookmark size={24}/>
-        <IoBagOutline size={24}/>
+        <CiSearch size={24} className="cursor-pointer"/>
+        <BsPerson size={24} className="cursor-pointer"/>
+        <CiBookmark size={24} className="cursor-pointer"/>
+        <IoBagOutline size={24} className="cursor-pointer"/>
         </div>
         </div> 
    <div className="flex items-center justify-center gap-4 text-sm p-4">
-    <span>Bags</span>
-    <span>Travel</span>
-    <span>Accessories</span>
-    <span>Gifting</span> 
-    <span>Jwelery</span>
+    <span className="cursor-pointer">Bags</span>
+    <span className="cursor-pointer">Travel</span>
+    <span className="cursor-pointer">Accessories</span>
+    <span className="cursor-pointer">Gifting</span> 
+    <span className="cursor-pointer">Jwelery</span>
    </div>
     </div>
   )
--- src/app/components/MainBody.js
+++ src/app/components/MainBody.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { GoDotFill } from "react-icons/go";
 import { GrUpload } from "react-icons/gr";
 import { fetchProducts } from '../lib/api';
+import { TbShoppingBagPlus } from "react-icons/tb";
+import { CiBookmark } from "react-icons/ci";
+import { MdCurrencyRupee } from "react-icons/md";
 
 const MainBody = async() => {
 
     const products = await fetchProducts();
 
   return (
-    <div className='flex flex-col'>
+    <div className='flex flex-col w-full h-full'>
         <div className='flex justify-between mt-6 text-sm lg:text-base'>
             <div className='flex items-center gap-2'>
                 <span>Bags</span>
@@ -20,19 +23,26 @@ const MainBody = async() => {
                 <GrUpload />
             </div>
         </div>
-    <div className='mt-8 mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 '>
+    <div className='mt-8 mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 transition-all duration-500'>
     {products.map((product) => (
-              <div key={product.id} className="h-[483px] w-[280px] mx-4 object-contain rounded-xl bg-[#141414]">
+              <div key={product.id} className="relative h-[500px] w-full max-w-[320px] mx-4 object-contain rounded-xl bg-[#141414] 
+              transition-transform duration-500 ease-in-out ">
                
                   <img
                     src={product.image}
                     alt={product.title}
-                    className="w-[290px] h-[400px] bg-white rounded-xl"
+                    className="w-full h-[350px] bg-white rounded-t-xl"
                   />
-                
-                <div>
-                  <h2>{product.title}</h2>
-                  <p>Rs {product.price}</p>
+                <CiBookmark size={35} className='cursor-pointer absolute top-2 right-2 text-black'/>
+                <div className='flex flex-col p-2'>
+                 <div className='w-full h-[50px]'> <p className='text-lg'>{product.title}</p></div>
+                  <div className='flex items-center justify-between mt-8'>
+                  <span className='flex items-center text-lg gap-2 font-medium'><MdCurrencyRupee size={20}/> {product.price} 
+                  <span className='line-through text-xs text-[#E5DFD9] font-thin'> {product.price*2}</span> 
+                  <span className='text-[#2FC14F] text-xs'>(50% Off)</span>
+                  </span>
+                  <TbShoppingBagPlus size={35} className='cursor-pointer'/>
+                  </div>
                 </div>
               </div>
             ))}
